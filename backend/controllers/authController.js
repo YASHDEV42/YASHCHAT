7de89aa4e58a3d0bb3e28a4fcd8bd38a2441399d
Yahya Shannat <yahyashannat@gmail.com>
2025-05-21 10:48:14 +0300

    initial commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "There is no such a user" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Password is incorrect" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({
+      token,
+      user: { id: user._id, username: user.username, email: user.email },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const socketSetup = require("./socket");
+
+dotenv.config();
+/*
+Purpose:
+It loads variables from a .env file into process.env.
+*/
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+/* 
+Purpose:
+Allows your frontend (e.g., on localhost:3000) to communicate with your backend (e.g., on localhost:5000), which are on different origins.
+Without CORS, the browser will block the requests due to same-origin policy.
+*/
+app.use(express.json());
+/*
+Purpose:
+Purpose:
+Tells Express to automatically parse incoming requests with Content-Type: application/json and make it available in req.body.
+*/
+
+app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/messages", require("./routes/messageRoutes"));
+
+// socketSetup(server);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+
+    server.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
